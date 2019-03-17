1b95b0f3811fa4674b6bac5d6166734102c1d666
uttxhy <haoyangxu123@gmail.com>
2019-03-17 09:49:02 +0100

    index.html修改

--- zhonghaosite/src/main/resources/static/js/index.js
+++ zhonghaosite/src/main/resources/static/js/index.js
@@ -1,6 +1,18 @@
-function getAjax() {
-    request().get('/api/home/bannersectioninit', function (res) {
-        console.log(res);
+$('#msgForm').on('submit', function () {
+    saveReport();
+    event.preventDefault();
+});
+function saveReport() {
+    var username = $('#username').val();
+    var email = $('#email').val();
+    var message = $('#message').val();
+    console.log(username);
+    $.ajax({
+        url: "home/sendMsg",
+        data: { username:username, email: email, message: message },
+        success: function (res) {
+            console.log(res);
+        }
     });
 }
-getAjax();
+// getAjax();
