9493a8cb5aa583b7f7a6f4b6672e18aa811bb5d1
runqitian <tianrunqi94@gmail.com>
2019-07-12 09:46:40 +0800

    dao finished untested

--- zhonghaosite/src/main/java/com/zhonghaollc/zhonghaosite/entity/about/Signature.java
+++ zhonghaosite/src/main/java/com/zhonghaollc/zhonghaosite/entity/about/Signature.java
@@ -1,4 +1,72 @@
 package com.zhonghaollc.zhonghaosite.entity.about;
 
 public class Signature {
+
+    private String title;
+    private  String text;
+    private String job;
+    private String person;
+    private String imgPath;
+
+    public Signature() {
+    }
+
+    public Signature(String title, String text, String job, String person, String imgPath) {
+        this.title = title;
+        this.text = text;
+        this.job = job;
+        this.person = person;
+        this.imgPath = imgPath;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public String getPerson() {
+        return person;
+    }
+
+    public void setPerson(String person) {
+        this.person = person;
+    }
+
+    public String getImgPath() {
+        return imgPath;
+    }
+
+    public void setImgPath(String imgPath) {
+        this.imgPath = imgPath;
+    }
+
+    @Override
+    public String toString() {
+        return "Signature{" +
+                "title='" + title + '\'' +
+                ", text='" + text + '\'' +
+                ", job='" + job + '\'' +
+                ", person='" + person + '\'' +
+                ", imgPath='" + imgPath + '\'' +
+                '}';
+    }
 }
--- zhonghaosite/src/main/java/com/zhonghaollc/zhonghaosite/service/UpdateService.java
+++ zhonghaosite/src/main/java/com/zhonghaollc/zhonghaosite/service/UpdateService.java
@@ -2,6 +2,8 @@ package com.zhonghaollc.zhonghaosite.service;
 
 
 import com.zhonghaollc.zhonghaosite.dao.S3AccessObject;
+import com.zhonghaollc.zhonghaosite.entity.about.AboutPage;
+import com.zhonghaollc.zhonghaosite.entity.contact.ContactPage;
 import com.zhonghaollc.zhonghaosite.entity.index.*;
 import freemarker.template.Configuration;
 import freemarker.template.Template;
@@ -24,66 +26,82 @@ public class UpdateService {
     public void updateIndex() {
         try {
             IndexPage indexPage = s3AccessObject.indexPageDao();
-//            System.out.println(indexPage);
 
-            // 1. Configure FreeMarker
-            //
-            // You should do this ONLY ONCE, when your application starts,
-            // then reuse the same Configuration object elsewhere.
-
-            Configuration cfg = new Configuration(Configuration.VERSION_2_3_28);
-
-            // Where do we load the templates from:
-            cfg.setClassForTemplateLoading(UpdateService.class, "/temporary/");
-
-            // Some other recommended settings:
-            cfg.setIncompatibleImprovements(new Version(2, 3, 20));
-            cfg.setDefaultEncoding("UTF-8");
-            cfg.setLocale(Locale.US);
-            cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
-
-            // 2. Proccess template(s)
-            //
-            // You will do this for several times in typical applications.
-
-            // 2.1. Prepare the template input:
+            Configuration cfg = setUpConfiguration();
 
             Map<String, Object> input = new HashMap<String, Object>();
             input.put("indexPage", indexPage);
 
-            // 2.2. Get the template
-
             Template template = cfg.getTemplate("index.html");
 
-            // 2.3. Generate the output
-
-            // Write output to the console
-//            Writer consoleWriter = new OutputStreamWriter(System.out);
-//            template.process(input, consoleWriter);
-            File tmpResource = new File(UpdateService.class.getClassLoader().getResource("").getPath() + File.separator + "templates");
-            if (!tmpResource.exists()) {
-                tmpResource.mkdir();
-            }
-
-            // For the sake of example, also write output into a file:
-            Writer fileWriter = new FileWriter(new File(tmpResource.getPath() + File.separator + "index.html"));
-            try {
-//                template.process(input, fileWriter);
-                template.process(input, fileWriter);
-            } finally {
-                fileWriter.close();
-            }
+            generateStatic(template, input, "index.html");
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     public void updateContact(){
+        try {
+            ContactPage contactPage = s3AccessObject.contactPageDao();
 
+            Configuration cfg = setUpConfiguration();
+
+            Map<String, Object> input = new HashMap<String, Object>();
+            input.put("contactPage", contactPage);
+
+            Template template = cfg.getTemplate("contact.html");
+
+            generateStatic(template, input, "contact.html");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    public void updateAbout(){
 
+    public void updateAbout(){
+        try {
+            AboutPage aboutPage = s3AccessObject.aboutPageDao();
+
+            Configuration cfg = setUpConfiguration();
+
+            Map<String, Object> input = new HashMap<String, Object>();
+            input.put("aboutPage", aboutPage);
+
+            Template template = cfg.getTemplate("about.html");
+
+            generateStatic(template, input, "about.html");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private Configuration setUpConfiguration(){
+        Configuration cfg = new Configuration(Configuration.VERSION_2_3_28);
+        cfg.setClassForTemplateLoading(UpdateService.class, "/temporary/");
+        cfg.setIncompatibleImprovements(new Version(2, 3, 20));
+        cfg.setDefaultEncoding("UTF-8");
+        cfg.setLocale(Locale.US);
+        cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
+        return cfg;
+    }
+
+    private void generateStatic(Template template, Map<String, Object> input, String savename) throws Exception{
+        File tmpResource = new File(UpdateService.class.getClassLoader().getResource("").getPath() + File.separator + "templates");
+        if (!tmpResource.exists()) {
+            tmpResource.mkdir();
+        }
+
+        // For the sake of example, also write output into a file:
+        Writer fileWriter = new FileWriter(new File(tmpResource.getPath() + File.separator + savename));
+        try {
+//                template.process(input, fileWriter);
+            template.process(input, fileWriter);
+        } finally {
+            fileWriter.close();
+        }
     }
 
 }
